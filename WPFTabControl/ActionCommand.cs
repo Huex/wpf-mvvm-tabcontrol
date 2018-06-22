c8e0f7aa23e5696383d0fbe6da03ab58154c556c
Huex <tohuex@gmail.com>
2018-06-22 17:04:53 +0300

    add ActionCommand

---
+++ WPFTabControl/ActionCommand.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WPFTabControl
+{
+    public class ActionCommand : ICommand
+    {
+        private readonly Action<object> action;
+        private readonly Predicate<Object> predicate;
+
+        public ActionCommand(Action<Object> action) : this(action, null)
+        {
+        }
+
+        public ActionCommand(Action<Object> action, Predicate<Object> predicate)
+        {
+            if (action == null)
+            {
+                throw new ArgumentNullException(nameof(action), @"You must specify an Action<T>.");
+            }
+
+            this.action = action;
+            this.predicate = predicate;
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add
+            {
+                CommandManager.RequerySuggested += value;
+            }
+            remove
+            {
+                CommandManager.RequerySuggested -= value;
+            }
+        }
+
+        public bool CanExecute(object parameter)
+        {
+            if (this.predicate == null)
+            {
+                return true;
+            }
+            return this.predicate(parameter);
+        }
+
+        public void Execute()
+        {
+            Execute(null);
+        }
+
+        public void Execute(object parameter)
+        {
+            this.action(parameter);
+        }
+    }
+}
